2c32fdddf8cd8d8d6f3431d48e9bc4ae1655da4d
Dellamora <francielle@dellamora.dev>
2023-02-28 15:06:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('form');
+      const newTaskInput = document.querySelector('#new-task');
+      const taskList = document.querySelector('#task-list');
+
+      form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        addTask(newTaskInput.value);
+        newTaskInput.value = '';
+      });
+
+      function addTask(taskText) {
+        const taskItem = document.createElement('li');
+        taskItem.textContent = taskText;
+        taskList.appendChild(taskItem);
+
+        taskItem.addEventListener('click', () => {
+          removeTask(taskItem);
+        });
+      }
+
+      function removeTask(taskItem) {
+        taskItem.parentNode.removeChild(taskItem);
+      }
